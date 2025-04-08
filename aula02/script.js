698ac993698a0d6b042aa2b0d74c328c98dda84b
JoaoG0988 <gabrielmachado.6677@gmail.com>
2025-04-07 22:07:13 -0300

    update aula02

--- aula02/script.js
+++ aula02/script.js
@@ -1,41 +1,40 @@
+function jogar(){
+    while(true){
+        idade = prompt("Quantos anos você tem?");
+        if(idade >= 18){
+            alert("Você pode jogar !");
+            player = prompt("Escolha: 1-PEDRA, 2-PAPEL OU 3-TESOURA?");
+            comp = Math.floor(Math.random() * 3) + 1;
 
-while(true){
-    idade = prompt("Quantos anos você tem?");
-    if(idade >= 18){
-        alert("Você pode jogar !");
-        player = prompt("Escolha: 1-PEDRA, 2-PAPEL OU 3-TESOURA?");
-        comp = Math.floor(Math.random() * 3) + 1;
-
-        if(player == comp){
-            alert("Empate!");
+            if(player == comp){
+                alert("Empate!");
+            }
+            else if(player == 1 && comp == 2){
+                alert("Você perdeu!");
+            }
+            else if(player == 1 && comp == 3){
+                alert("Você ganhou!");
+            }
+            else if(player == 2 && comp == 1){
+                alert("Você ganhou!");
+            }
+            else if(player == 2 && comp == 3){
+                alert("Você perdeu!");
+            }
+            else if(player == 3 && comp == 1){
+                alert("Você perdeu!");
+            }
+            else if(player == 3 && comp == 2){
+                alert("Você ganhou!");
+            }
+            else{
+                alert("Opção inválida!");
+            }
         }
-        else if(player == 1 && comp == 2){
-            alert("Você perdeu!");
+        if(idade < 18){
+            alert("Você não pode jogar !");
+            break;
         }
-        else if(player == 1 && comp == 3){
-            alert("Você ganhou!");
-        }
-        else if(player == 2 && comp == 1){
-            alert("Você ganhou!");
-        }
-        else if(player == 2 && comp == 3){
-            alert("Você perdeu!");
-        }
-        else if(player == 3 && comp == 1){
-            alert("Você perdeu!");
-        }
-        else if(player == 3 && comp == 2){
-            alert("Você ganhou!");
-        }
-        else{
-            alert("Opção inválida!");
-        }
+        alert("Escolha do computador: " + comp);
     }
-    if(idade < 18){
-        alert("Você não pode jogar !");
-        break;
-    }
-    alert("Escolha do computador: " + comp);
-   
 }
-
